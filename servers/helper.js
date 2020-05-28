90607728f5a02d022ca368c947cb4c7d2407a180
neo <sjkyspa@gmail.com>
2020-05-28 20:43:20 +0800

    refactor: refactor the data file format

--- servers/helper.js
+++ servers/helper.js
@@ -4,6 +4,14 @@ json.register = function (Handlebars) {
     Handlebars.registerHelper('json', function(obj) {
 	return JSON.stringify(obj, null, 2);
     });
+
+    Handlebars.registerHelper('protocol', function(list, name, opts) {
+	var i, result = '';
+	for(i = 0; i < list.length; ++i)
+            if(list[i]["name"] == name)
+		result = result + opts.fn(list[i]);
+	return result;
+    });
 };
 
 module.exports = json;
