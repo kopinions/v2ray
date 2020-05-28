4cb917e36a3b51f4c69cba0ee5845d75e1b5291e
neo <sjkyspa@gmail.com>
2020-05-28 20:59:21 +0800

    fix: fix template error

--- servers/helper.js
+++ servers/helper.js
@@ -5,7 +5,7 @@ json.register = function (Handlebars) {
 	return JSON.stringify(obj, null, 2);
     });
 
-    Handlebars.registerHelper('protocol', function(list, name, opts) {
+    Handlebars.registerHelper('select', function(list, name, opts) {
 	var i, result = '';
 	for(i = 0; i < list.length; ++i)
             if(list[i]["name"] == name)
